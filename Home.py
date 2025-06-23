26851026803bd27ae5d1a879945a9f7be8484516
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-24 02:55:39 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("🎓 Aplikasi Kuis Matematika SD")
+st.markdown("""
+Selamat datang di aplikasi kuis Matematika interaktif!
+
+Pilih menu:
+- 🧮 **Kuis Matematika SD**: untuk siswa
+- 🔐 **Admin Dashboard**: untuk guru / admin melihat data
+
+""")
