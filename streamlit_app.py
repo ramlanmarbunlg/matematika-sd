0fe8c5e9f30ba0602573712e43170bff7be03ee2
Ramlan Marbun <86874981+ramlanmarbunlg@users.noreply.github.com>
2025-06-24 04:06:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -188,25 +188,25 @@ if st.session_state.index_soal >= len(st.session_state.soal_acak):
     if st.button("📊 Lihat Statistik Belajar"):
         tampilkan_statistik()
 
-     def buat_sertifikat(nama, kelas, skor, total):
-        buffer = BytesIO()
-        c = canvas.Canvas(buffer)
-        c.setFillColor(colors.lightblue)
-        c.rect(50, 500, 740, 50, fill=1)
-        c.setFillColor(colors.black)
-        c.setFont("Helvetica-Bold", 22)
-        c.drawCentredString(421, 520, "SERTIFIKAT KUIS MATEMATIKA")
-        c.setFont("Helvetica", 14)
-        c.drawString(100, 700, f"Nama: {nama}")
-        c.drawString(100, 680, f"Kelas: {kelas}")
-        c.drawString(100, 660, f"Skor: {skor} dari {total}")
-        
-        tanggal = datetime.now(ZoneInfo("Asia/Jakarta")).strftime('%d %B %Y')
-        c.drawString(100, 640, f"Tanggal: {tanggal}")
-        
-        c.save()
-        buffer.seek(0)
-        return buffer
+         def buat_sertifikat(nama, kelas, skor, total):
+            buffer = BytesIO()
+            c = canvas.Canvas(buffer)
+            c.setFillColor(colors.lightblue)
+            c.rect(50, 500, 740, 50, fill=1)
+            c.setFillColor(colors.black)
+            c.setFont("Helvetica-Bold", 22)
+            c.drawCentredString(421, 520, "SERTIFIKAT KUIS MATEMATIKA")
+            c.setFont("Helvetica", 14)
+            c.drawString(100, 700, f"Nama: {nama}")
+            c.drawString(100, 680, f"Kelas: {kelas}")
+            c.drawString(100, 660, f"Skor: {skor} dari {total}")
+            
+            tanggal = datetime.now(ZoneInfo("Asia/Jakarta")).strftime('%d %B %Y')
+            c.drawString(100, 640, f"Tanggal: {tanggal}")
+            
+            c.save()
+            buffer.seek(0)
+            return buffer
 
 with st.expander("⚠️ Opsi Admin: Hapus Semua Skor"):
     if st.button("🗑️ Hapus Semua Data Skor"):
